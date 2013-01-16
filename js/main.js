b3fd80386e9b4fa0b71a842bf94a7337ba0d20cc
Scott Kirkland <srkirkland@gmail.com>
2013-01-16 14:01:23 -0800

    custom code instead of google spreadsheet plugin

--- js/main.js
+++ js/main.js
@@ -1,32 +1,18 @@
-//var sampleUrl = "https://docs.google.com/spreadsheet/pub?key=0AhcpJCFiGkDmdFZOSXpGMDVSa1N4NWJtRG8wWmx4YUE&single=true&gid=0&output=html";
-var sampleUrl = "https://docs.google.com/spreadsheet/pub?key=0AhTxmYCYi3fpdHI5RnliaG1yMGZxeEVTYnJXc1Fxb3c&single=true&gid=0&output=html";
 var spreadsheetData = [];
-var headerData = [];
-        
-$(function() {
-    var googleSpreadsheet = new GoogleSpreadsheet();
-    googleSpreadsheet.url(sampleUrl);
-    googleSpreadsheet.load(function(result) {
-        //break cells up into rows/cols by given column count
-        var colCount = 7;
-        for (var i = 0; i < result.data.length; i) {
-            var row = [];
-            for (var j = 0; j < colCount; j++) {
-                row.push(result.data[i + j]);
-            }
-            spreadsheetData.push(row);
-            i = i + colCount;
-        }
-                
-        var header = spreadsheetData.splice(0, 1); //splice first header row off
-        for (var col in header[0]) {
-            headerData.push({ "sTitle": header[0][col] });
-        }
+var headerData = [{ "sTitle": "Researcher Name" }, { "sTitle": "College" }, { "sTitle": "Department"}];
 
-        $('#results').html(JSON.stringify(result).replace(/,/g, ",\n"));
+$(function () {
+    var url = "https://spreadsheets.google.com/feeds/list/0AhTxmYCYi3fpdHI5RnliaG1yMGZxeEVTYnJXc1Fxb3c/1/public/values?alt=json-in-script&callback=?";
+    $.getJSON(url, {}, function (data) {
+        $.each(data.feed.entry, function (key, val) {
+            spreadsheetData.push([val.gsx$researchername.$t, val.gsx$college.$t, val.gsx$department.$t]);
+        });
+
+        console.log(spreadsheetData);
         createDataTable();
     });
-});
+})
+
 
 function createDataTable() {
     $("#spreadsheet").dataTable({
