6acce9bdfa7f83d3df9e433bbd9dd3c0e4135e5b
Scott Kirkland <srkirkland@gmail.com>
2013-01-16 14:37:14 -0800

    scope variables and added unique college list

--- js/main.js
+++ js/main.js
@@ -1,31 +1,43 @@
-var spreadsheetData = [];
-var headerData = [
+MyApp = {};
+MyApp.spreadsheetData = [];
+MyApp.headerData = [
     { "sTitle": "Researcher Name" }, { "sTitle": "College" }, 
     { "sTitle": "Department"}, { "sTitle": "Researcher Title"}, 
     { "sTitle": "Website"}, { "sTitle": "Email"}, 
     { "sTitle": "Keywords"}
 ];
 
+MyApp.Colleges = [];
+
 $(function () {
     var url = "https://spreadsheets.google.com/feeds/list/0AhTxmYCYi3fpdHI5RnliaG1yMGZxeEVTYnJXc1Fxb3c/1/public/values?alt=json-in-script&callback=?";
     $.getJSON(url, {}, function (data) {
         $.each(data.feed.entry, function (key, val) {
-            spreadsheetData.push(
+            var college = val.gsx$college.$t;
+
+            MyApp.spreadsheetData.push(
                 [
-                    val.gsx$researchername.$t, val.gsx$college.$t, 
-                    val.gsx$department.$t, val.gsx$researchertitle.$t, 
-                    val.gsx$website.$t, val["gsx$e-mail"].$t, 
+                    val.gsx$researchername.$t, college,
+                    val.gsx$department.$t, val.gsx$researchertitle.$t,
+                    val.gsx$website.$t, val["gsx$e-mail"].$t,
                     val.gsx$keywords.$t
                 ]);
+
+            if ($.inArray(college, MyApp.Colleges) === -1) {
+                MyApp.Colleges.push(college);
+            }
         });
 
+        MyApp.Colleges.sort();
+                
+        console.log(MyApp.Colleges);
         createDataTable();
     });
 })
 
 function createDataTable() {
-    $("#spreadsheet").dataTable({
-        "aaData": spreadsheetData,
-        "aoColumns": headerData
+    MyApp.oTable = $("#spreadsheet").dataTable({
+        "aaData": MyApp.spreadsheetData,
+        "aoColumns": MyApp.headerData
     });
 }
