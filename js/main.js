e480a105baef416b9f02bbb64b66c5674f7df5c3
Scott Kirkland <srkirkland@gmail.com>
2013-01-16 14:06:39 -0800

    finished adding remaining columns

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
 var spreadsheetData = [];
-var headerData = [{ "sTitle": "Researcher Name" }, { "sTitle": "College" }, { "sTitle": "Department"}];
+var headerData = [
+    { "sTitle": "Researcher Name" }, { "sTitle": "College" }, 
+    { "sTitle": "Department"}, { "sTitle": "Researcher Title"}, 
+    { "sTitle": "Website"}, { "sTitle": "Email"}, 
+    { "sTitle": "Keywords"}
+];
 
 $(function () {
     var url = "https://spreadsheets.google.com/feeds/list/0AhTxmYCYi3fpdHI5RnliaG1yMGZxeEVTYnJXc1Fxb3c/1/public/values?alt=json-in-script&callback=?";
     $.getJSON(url, {}, function (data) {
         $.each(data.feed.entry, function (key, val) {
-            spreadsheetData.push([val.gsx$researchername.$t, val.gsx$college.$t, val.gsx$department.$t]);
+            spreadsheetData.push(
+                [
+                    val.gsx$researchername.$t, val.gsx$college.$t, 
+                    val.gsx$department.$t, val.gsx$researchertitle.$t, 
+                    val.gsx$website.$t, val["gsx$e-mail"].$t, 
+                    val.gsx$keywords.$t
+                ]);
         });
 
-        console.log(spreadsheetData);
         createDataTable();
     });
 })
-
 
 function createDataTable() {
     $("#spreadsheet").dataTable({
